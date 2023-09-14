cf1b48193e070a5a22a404249c2b6660659388c9
Daniel Corn <daniel@iresults.li>
2023-09-14 18:50:53 +0200

    Register the Fluid namespace `iresultsResponsiveImages` globally

---
+++ ext_localconf.php
@@ -0,0 +1,6 @@
+<?php
+
+defined('TYPO3') || die();
+(function () {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['iresultsResponsiveImages'] = ['Iresults\\ResponsiveImages\\ViewHelpers'];
+})();
