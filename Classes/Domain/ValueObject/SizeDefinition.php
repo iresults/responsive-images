045a0a531d6872865ded12edd015368ac7644f00
Daniel Corn <daniel@iresults.li>
2026-03-09 14:51:07 +0100

    Validate `unit` property of `SizeDefinition`

--- Classes/Domain/ValueObject/SizeDefinition.php
+++ Classes/Domain/ValueObject/SizeDefinition.php
@@ -4,25 +4,46 @@ declare(strict_types=1);
 
 namespace Iresults\ResponsiveImages\Domain\ValueObject;
 
+use InvalidArgumentException;
 use Iresults\ResponsiveImages\ArrayUtility;
 
 final readonly class SizeDefinition
 {
+    /**
+     * @param ''|'px' $unit
+     */
     private function __construct(
         public string $mediaCondition,
         public float $imageWidth,
         public string $unit,
         public bool $isDefault,
     ) {
+        if ('px' !== $unit && '' !== $unit) {
+            throw new InvalidArgumentException(sprintf(
+                'Unit "%s" is not allowed',
+                $unit
+            ));
+        }
     }
 
-    public static function withMediaCondition(string $mediaCondition, float $imageWidth, string $unit): self
-    {
+    /**
+     * @param 'px'|'' $unit
+     */
+    public static function withMediaCondition(
+        string $mediaCondition,
+        float $imageWidth,
+        string $unit,
+    ): self {
         return new SizeDefinition($mediaCondition, $imageWidth, $unit, false);
     }
 
-    public static function defaultSizeDefinition(float $imageWidth, string $unit): self
-    {
+    /**
+     * @param 'px'|'' $unit
+     */
+    public static function defaultSizeDefinition(
+        float $imageWidth,
+        string $unit,
+    ): self {
         return new SizeDefinition('', $imageWidth, $unit, true);
     }
 
